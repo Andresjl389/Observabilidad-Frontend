5cc08fb235f857473278a0fa4dcee42dcb579289
Andresjl389 <andresjaimes195@gmail.com>
2025-07-19 16:40:58 -0500

    fix: add nginx.conf for cloud run port 8080

--- src/environment/local.ts
+++ src/environment/local.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  app_url: process.env.REACT_APP_API_URL || 'http://localhost:8000'
+  app_url: process.env.REACT_APP_API_URL || 'https://observabilidad-194225173776.europe-west1.run.app/.a.run.app'
 }
